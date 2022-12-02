0be5c036e18d55f78e1c709a5822fba03d1fb8f5
Dener Araujo (Natsume) <natsume2b.me@gmail.com>
2022-12-02 10:02:04 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import './styles/global.css'
+
+export function App() {
+
+  return (
+    <div>
+      <h1>Henlo Warudo!!!</h1>
+    </div>
+  )
+}
---
+++ tailwind.config.cjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.tsx",
+    "./index.html"
+  ],
+  theme: {
+    fontFamily: {
+      'Baloo-2': ['Baloo-2','cursive'],
+      'Roboto':['Roboto', 'sans-serif']
+      
+    },
+    extend: {},
+  },
+  plugins: [],
+}
