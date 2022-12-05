6ddbbddeec4b65aac3c8e752242b02c61226ac18
Dener Araujo (Natsume) <natsume2b.me@gmail.com>
2022-12-05 11:23:39 -0300

    Update Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import { MapPin, ShoppingCart } from 'phosphor-react'
 
 export function Header() {
   return (
-    <header className='flex justify-between items-center mt-8 '>
+    <header className='flex justify-between items-center mt-8 mb-24 '>
       <a href="/"><img src={coffee} alt="Go to home" title='Home' className='cursor-pointer' /></a>
       <div className='flex gap-3 content-center'>
         <div className='flex items-center gap-1 bg-product-purple-light p-2 rounded-md '>
@@ -14,7 +14,7 @@ export function Header() {
             Porto Alegre, RS
           </span>
         </div>
-        <span className='text-product-yellow-dark bg-product-yellow-light p-2 rounded-md'>
+        <span className='text-product-yellow-dark bg-product-yellow-light p-2 rounded-md cursor-pointer'>
           <ShoppingCart size={22} weight="fill" />
         </span>
       </div>
