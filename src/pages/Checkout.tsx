622b941bbb2d4d1c11d06f555d3673ef86048d26
Dener Araujo (Natsume) <natsume2b.me@gmail.com>
2022-12-12 16:06:48 -0300

    Update 'endereco de entrega' on checkout

--- src/pages/Checkout.tsx
+++ src/pages/Checkout.tsx
@@ -18,12 +18,12 @@ export function Checkout() {
             </header>
           </div>
 
-          <form action="" className='grid grid-cols-3'>
+          <form action="" className='grid grid-cols-[30%_55%_15%] gap-3'>
 
             <input type="number"
               placeholder="CEP"
               name="cep"
-              className='mb-4 p-3 rounded  bg-base-input border border-base-button text-base-label'
+              className='mb-1 p-3 rounded row-start-1 bg-base-input border border-base-button text-base-label'
             />
             <label htmlFor="cep"></label>
 
@@ -31,7 +31,7 @@ export function Checkout() {
               type="text"
               placeholder="Rua"
               name="rua"
-              className='mb-4 p-3 rounded col-start-1 col-span-3 bg-base-input border border-base-button text-base-label'
+              className='mb-1 p-3 rounded row-start-2 col-span-3  bg-base-input border border-base-button text-base-label'
             />
             <label htmlFor="rua"></label>
 
@@ -40,7 +40,7 @@ export function Checkout() {
               type="number"
               placeholder="Número"
               name="numero"
-              className='mb-4 p-3 rounded col-start-1 row-start-3 bg-base-input border border-base-button text-base-label'
+              className='mb-1 p-3 rounded row-start-3  bg-base-input border border-base-button text-base-label'
             />
             <label htmlFor="numero"></label>
 
@@ -48,7 +48,7 @@ export function Checkout() {
               type="text"
               placeholder="Complemento"
               name="complemento"
-              className='mb-4 p-3 ml-3 rounded col-start-2 row-start-3 col-span-2 bg-base-input border border-base-button text-base-label'
+              className='mb-1 p-3 rounded row-start-3  bg-base-input border border-base-button text-base-label'
             />
             <label htmlFor="complemento"></label>
 
@@ -56,7 +56,7 @@ export function Checkout() {
               type="text"
               placeholder="Bairro"
               name="bairro"
-              className='mb-4 p-3 rounded col-start-1 row-start-5 bg-base-input border border-base-button text-base-label'
+              className='mb-1 p-3 rounded row-start-4 bg-base-input border border-base-button text-base-label'
             />
             <label htmlFor="bairro"></label>
 
@@ -64,7 +64,7 @@ export function Checkout() {
               type="text"
               placeholder="Cidade"
               name="cidade"
-              className='mb-4 p-3 rounded ml-3 col-start-2 row-start-5 w-[146.5%] bg-base-input border border-base-button text-base-label'
+              className='mb-1 p-3 rounded row-start-4 bg-base-input border border-base-button text-base-label'
             />
             <label htmlFor="cidade"></label>
 
@@ -72,7 +72,7 @@ export function Checkout() {
               type="text"
               placeholder="UF"
               name="uf"
-              className='mb-4 p-3 rounded ml-[calc(53.5%_+_0.75rem)] max-w-max col-start-3 row-start-5 w-2/5 bg-base-input border border-base-button text-base-label'
+              className='mb-1 p-3 rounded row-start-4 bg-base-input border border-base-button text-base-label'
             />
             <label htmlFor="uf"></label>
 
