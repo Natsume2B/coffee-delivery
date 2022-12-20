26e12cfd6f531f16b5f2a4b6821a221e80f1748e
Dener Araujo (Natsume) <natsume2b.me@gmail.com>
2022-12-20 19:12:04 -0300

    Update Products and CoffeeCard

--- src/components/CoffeeCard.tsx
+++ src/components/CoffeeCard.tsx
@@ -2,10 +2,12 @@ import { ShoppingCart } from "phosphor-react"
 import { Plus } from 'phosphor-react'
 import { Minus } from 'phosphor-react'
 import { useState } from "react"
+import { Type1 } from "./CoffeeTypes"
 
 interface CoffeeProductProps {
   src: string,
-  coffeeType: string,
+  numberOfType: string,
+  coffeeType1: string,
   coffeeType2?: string,
   coffeeType3?: string,
   name: string,
@@ -14,7 +16,7 @@ interface CoffeeProductProps {
 }
 
 
-export function CoffeeCard1Type({ src, coffeeType, name, about, price }: CoffeeProductProps) {
+export function CoffeeCard1Type({ src, numberOfType, coffeeType1, coffeeType2, coffeeType3, name, about, price }: CoffeeProductProps) {
   const [productCount, setProductCount] = useState(0)
 
   function increaseProductCount() {
@@ -29,106 +31,37 @@ export function CoffeeCard1Type({ src, coffeeType, name, about, price }: CoffeeP
     }
   }
 
-  return (
-    <div className="flex flex-col items-center text-center w-[22%] rounded-tr-[2.250rem] rounded-bl-[2.250rem] rounded-tl-md rounded-br-md bg-base-card mb-5">
-      <img className="mt-[-1.45rem] mb-3" src={src} alt="" />
-      <div className="flex flex-row gap-1">
-        <span className="text-[10px] font-bold p-1 px-2 rounded-full bg-product-yellow-light text-product-yellow-dark">{coffeeType}</span>
-      </div>
-      <h1 className="font-baloo2 text-xl mb-2">{name}</h1>
-      <p className="text-sm text-base-label mb-8 px-1">{about}</p>
-      <div className="flex items-center mb-5">
-        <span className="text-sm mr-1">R$</span>
-        <span className="font-baloo2 text-2xl mr-6"> {price}</span>
-        <div className="flex gap-3 mr-2 p-2  rounded-md bg-base-button">
-          <button
-            className="text-product-purple hover:text-product-purple-dark"
-            onClick={decreaseProductCount}>
-            <Minus weight="bold" size={14} />
-          </button>
-          <span>{productCount as number}</span>
-          <button
-            className="text-product-purple hover:text-product-purple-dark"
-            onClick={increaseProductCount}>
-            <Plus weight="bold" size={14} />
-          </button>
+  function coffeeTypeText() {
+    if (numberOfType === '1') {
+      return (
+        <div className="flex flex-row gap-1 mb-4">
+          <span className="text-[10px] font-bold p-1 px-2 rounded-full bg-product-yellow-light text-product-yellow-dark">{coffeeType1}</span>
         </div>
-        <button className="rounded-md text-base-card p-2 bg-product-purple-dark hover:bg-product-purple"><ShoppingCart weight="fill" size={22} /></button>
-      </div>
-    </div>
-  )
-}
-
-export function CoffeeCard2Type({ src, coffeeType, coffeeType2, name, about, price }: CoffeeProductProps) {
-  const [productCount, setProductCount] = useState(0)
-
-  function increaseProductCount() {
-    if (productCount >= 0) {
-      setProductCount(productCount + 1)
-    }
-  }
-
-  function decreaseProductCount() {
-    if (productCount >= 1) {
-      setProductCount(productCount - 1)
-    }
-  }
-
-  return (
-    <div className="flex flex-col items-center text-center w-[22%] rounded-tr-[2.250rem] rounded-bl-[2.250rem] rounded-tl-md rounded-br-md bg-base-card mb-5">
-      <img className="mt-[-1.45rem] mb-3" src={src} alt="" />
-      <div className="flex flex-row gap-1">
-        <span className="text-[10px] font-bold p-1 px-2 rounded-full bg-product-yellow-light text-product-yellow-dark">{coffeeType}</span>
-        <span className="text-[10px] font-bold p-1 px-2 rounded-full bg-product-yellow-light text-product-yellow-dark">{coffeeType2}</span>
-      </div>
-      <h1 className="font-baloo2 text-xl mb-2">{name}</h1>
-      <p className="text-sm text-base-label mb-8 px-1">{about}</p>
-      <div className="flex items-center mb-5">
-        <span className="text-sm mr-1">R$</span>
-        <span className="font-baloo2 text-2xl mr-6"> {price}</span>
-        <div className="flex gap-3 mr-2 p-2  rounded-md bg-base-button">
-          <button
-            className="text-product-purple hover:text-product-purple-dark"
-            onClick={decreaseProductCount}>
-            <Minus weight="bold" size={14} />
-          </button>
-          <span>{productCount}</span>
-          <button
-            className="text-product-purple hover:text-product-purple-dark"
-            onClick={increaseProductCount}>
-            <Plus weight="bold" size={14} />
-          </button>
+      )
+    } else if (numberOfType === '2') {
+      return (
+        <div className="flex flex-row gap-1 mb-4">
+          <span className="text-[10px] font-bold p-1 px-2 rounded-full bg-product-yellow-light text-product-yellow-dark">{coffeeType1}</span>
+          <span className="text-[10px] font-bold p-1 px-2 rounded-full bg-product-yellow-light text-product-yellow-dark">{coffeeType2}</span>
         </div>
-        <button className="rounded-md text-base-card p-2 bg-product-purple-dark hover:bg-product-purple"><ShoppingCart weight="fill" size={22} /></button>
-      </div>
-    </div>
-  )
-}
+      )
 
-export function CoffeeCard3Type({ src, coffeeType, coffeeType2, coffeeType3, name, about, price }: CoffeeProductProps) {
-  const [productCount, setProductCount] = useState(0)
+    } else if (numberOfType === '3') {
+      return (
+        <div className="flex flex-row gap-1 mb-4">
+          <span className="text-[10px] font-bold p-1 px-2 rounded-full bg-product-yellow-light text-product-yellow-dark">{coffeeType1}</span>
+          <span className="text-[10px] font-bold p-1 px-2 rounded-full bg-product-yellow-light text-product-yellow-dark">{coffeeType2}</span>
+          <span className="text-[10px] font-bold p-1 px-2 rounded-full bg-product-yellow-light text-product-yellow-dark">{coffeeType3}</span>
+        </div>
+      )
 
-  function increaseProductCount() {
-    if (productCount >= 0) {
-      setProductCount(productCount + 1)
     }
   }
-
-  function decreaseProductCount() {
-    if (productCount >= 1) {
-      setProductCount(productCount - 1)
-    }
-  }
-
 
   return (
     <div className="flex flex-col items-center text-center w-[22%] rounded-tr-[2.250rem] rounded-bl-[2.250rem] rounded-tl-md rounded-br-md bg-base-card mb-5">
       <img className="mt-[-1.45rem] mb-3" src={src} alt="" />
-      <div className="flex flex-row gap-1">
-        <span className="text-[10px] font-bold p-1 px-2 rounded-full bg-product-yellow-light text-product-yellow-dark">{coffeeType}</span>
-        <span className="text-[10px] font-bold p-1 px-2 rounded-full bg-product-yellow-light text-product-yellow-dark">{coffeeType2}</span>
-        <span className="text-[10px] font-bold p-1 px-2 rounded-full bg-product-yellow-light text-product-yellow-dark">{coffeeType3}</span>
-      </div>
+      {coffeeTypeText()}
       <h1 className="font-baloo2 text-xl mb-2">{name}</h1>
       <p className="text-sm text-base-label mb-8 px-1">{about}</p>
       <div className="flex items-center mb-5">
--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -13,91 +13,102 @@ import cubano from '../assets/coffees/cubano.svg'
 import havaiano from '../assets/coffees/havaiano.svg'
 import arabe from '../assets/coffees/arabe.svg'
 import irlandes from '../assets/coffees/irlandes.svg'
-import { CoffeeCard1Type, CoffeeCard2Type, CoffeeCard3Type } from './CoffeeCard'
+import { CoffeeCard1Type } from './CoffeeCard'
 
 export function Products() {
   return (
     <div className='flex flex-wrap gap-8 '>
       <CoffeeCard1Type
         src={expressoTradicional}
-        coffeeType='TRADICIONAL'
+        numberOfType='1'
+        coffeeType1='TRADICIONAL'
         name='Expresso Tradicional'
         about='O tradicional café feito com água quente e grãos moídos'
         price='9,90'
       />
       <CoffeeCard1Type
         src={expressoAmericano}
-        coffeeType='TRADICIONAL'
+        numberOfType='1'
+        coffeeType1='TRADICIONAL'
         name='Expresso Ameriano'
         about='Expresso diluído, menos intenso que o tradicional'
         price='9,90'
       />
       <CoffeeCard1Type
         src={expressoCremoso}
-        coffeeType='TRADICIONAL'
+        numberOfType='1'
+        coffeeType1='TRADICIONAL'
         name='Expresso Cremoso'
         about='Café expresso tradicional com espuma cremosa'
         price='9,90'
       />
-      <CoffeeCard2Type
+      <CoffeeCard1Type
         src={expressoGelado}
-        coffeeType='Tradicional'
+        numberOfType='2'
+        coffeeType1='TRADICIONAL'
         coffeeType2='GELADO'
         name='Expresso Gelado'
         about='Bebida preparada com café expresso e cubos de gelo'
         price='9,90'
       />
-      <CoffeeCard2Type
+      <CoffeeCard1Type
         src={cafeComLeite}
-        coffeeType='TRADICIONAL'
+        numberOfType='2'
+        coffeeType1='TRADICIONAL'
         coffeeType2='COM LEITE'
         name='Café com Leite'
         about='Meio a meio de expresso tradicional com leite vaporizado'
         price='9,90'
       />
-      <CoffeeCard2Type
+      <CoffeeCard1Type
         src={latte}
-        coffeeType='TRADICIONAL'
+        numberOfType='2'
+        coffeeType1='TRADICIONAL'
         coffeeType2='COM LEITE'
         name='Latte'
         about='Uma dose de café expresso com o dobro de leite e espuma cremosa'
         price='9,90'
       />
-      <CoffeeCard2Type
+      <CoffeeCard1Type
         src={capuccino}
-        coffeeType='TRADICIONAL'
+        numberOfType='2'
+        coffeeType1='TRADICIONAL'
         coffeeType2='COM LEITE'
         name='Capuccino'
         about='Bebida com canela feita de doses iguais de café, leite e espuma'
         price='9,90'
       />
-      <CoffeeCard2Type
+      <CoffeeCard1Type
         src={macchiato}
-        coffeeType='TRADICIONAL'
+        numberOfType='2'
+        coffeeType1='TRADICIONAL'
         coffeeType2='COM LEITE'
         name='Macchiato'
         about='Café expresso misturado com um pouco de leite quente e espuma'
         price='9,90'
       />
-      <CoffeeCard2Type
+      <CoffeeCard1Type
         src={mocaccino}
-        coffeeType='TRADICIONAL'
+        numberOfType='2'
+        coffeeType1='TRADICIONAL'
         coffeeType2='COM LEITE'
         name='Mocaccino'
         about='Café expresso com calda de chocolate, pouco leite e espuma'
         price='9,90'
       />
-      <CoffeeCard2Type
+      <CoffeeCard1Type
         src={chocolateQuente}
-        coffeeType='ESPECIAL COM LEITE'
+        numberOfType='2'
+        coffeeType1='ESPECIAL COM LEITE'
         coffeeType2='COM LEITE'
         name='Chocolate Quente'
         about='Bebida feita com chocolate dissolvido no leite quente e café'
         price='9,90'
       />
-      <CoffeeCard3Type
+      <CoffeeCard1Type
         src={cubano}
-        coffeeType='ESPECIAL'
+        numberOfType='3'
+        coffeeType1='ESPECIAL'
         coffeeType2='ALCOÓLICO'
         coffeeType3='GELADO'
         name='Cubano'
@@ -106,21 +117,24 @@ export function Products() {
       />
       <CoffeeCard1Type
         src={havaiano}
-        coffeeType='ESPECIAL'
+        numberOfType='1'
+        coffeeType1='ESPECIAL'
         name='Havaiano'
         about='Bebida adocicada preparada com café e leite de coco'
         price='9,90'
       />
       <CoffeeCard1Type
         src={arabe}
-        coffeeType='ESPECIAL'
+        numberOfType='1'
+        coffeeType1='ESPECIAL'
         name='Árabe'
         about='Bebida preparada com grãos de café árabe e especiarias'
         price='9,90'
       />
-      <CoffeeCard2Type
+      <CoffeeCard1Type
         src={irlandes}
-        coffeeType='ESPECIAL'
+        numberOfType='2'
+        coffeeType1='ESPECIAL'
         coffeeType2='ALCOÓLICO'
         name='Irlandês'
         about='Bebida a base de café, uísque irlandês, açúcar e chantilly'
